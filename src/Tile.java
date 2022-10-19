c04a67a3832cbd92ba1b7fae247cfdf6442d2573
Haghighi <83563347+jaydonhaghighi@users.noreply.github.com>
2022-10-19 02:16:32 -0400

    Implemented Tile and Bag

---
+++ src/Tile.java
@@ -0,0 +1,25 @@
+public class Tile {
+    private String letter;
+    private Integer value;
+
+    public Tile(String letter, Integer value) {
+        this.letter = letter;
+        this.value = value;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Tile{" +
+                "letter='" + letter + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
