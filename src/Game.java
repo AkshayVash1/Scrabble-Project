cd1fc3f5422f94930b1b5d5a1a357ab6bda6e7c1
Akshay <akshayvashisht15@gmail.com>
2022-10-24 22:11:06 -0400

    Game class command check loop.

--- src/Game.java
+++ src/Game.java
@@ -8,7 +8,8 @@ public class Game {
     private Board board = new Board();
     private ArrayList<Player> playerList = new ArrayList<Player>();
 
-    public Game() {}
+    public Game() {
+    }
 
     private void createPlayers(String input) {
         int value = Integer.parseInt(input);
@@ -27,8 +28,7 @@ public class Game {
         if (!command.hasAction()) {
             System.out.println("Unknown Command");
             return false;
-        }
-        else if (command.hasWordAttempt()) {
+        } else if (command.hasWordAttempt()) {
             inHand = new InHand(command.getWordAttempt(), currentPlayer.getHand());
         }
 
@@ -42,8 +42,7 @@ public class Game {
                     addTilesToHand = this.bag.removeTiles(removeTilesFromHand.size());
                     bag.placeTiles(currentPlayer.exchange((ArrayList<Tile>) addTilesToHand,
                             removeTilesFromHand));
-                }
-                else {
+                } else {
                     return false;
                 }
                 break;
@@ -58,11 +57,10 @@ public class Game {
                             currentPlayer.exchange((ArrayList<Tile>) addTilesToHand, removeTilesFromHand),
                             this.board, command.getPlacementDirection());
 
-                    if (playMove.placeTile() == true) {
+                    if (playMove.placeTile()) {
                         this.board = playMove.getUpdatedBoard();
                         System.out.println("TRUE PLAY???");
-                    }
-                    else {
+                    } else {
                         System.out.println("FALSE PLAY???");
                         this.bag.placeTiles(addTilesToHand);
                         System.out.println("BIG SIZE AFTER " + this.bag.getBagSize());
@@ -80,7 +78,7 @@ public class Game {
 
 
             case "forfeit":
-                System.out.println("Player " + (currentPlayer.getPlayerNumber()+1) + " has forfeited");
+                System.out.println("Player " + (currentPlayer.getPlayerNumber() + 1) + " has forfeited");
                 bag.placeTiles(currentPlayer.getHand().getHand());
                 playerList.set(currentPlayer.getPlayerNumber(), null);
                 break;
@@ -112,63 +110,63 @@ public class Game {
 
         System.out.println("Welcome to Scrabble!");
 
-        while(!flag) {
+        while (!flag) {
             System.out.println("Type number of players (2, 3, or 4):");
             userInput = scanner.nextLine();
-            if (userInput.equals("2") | userInput.equals("3") | userInput.equals("4") ) {
+            if (userInput.equals("2") | userInput.equals("3") | userInput.equals("4")) {
                 game.createPlayers(userInput);
                 flag = true;
-            }
-            else {
+            } else {
                 System.out.println("Not a valid input, please try again.");
             }
         }
 
         flag = false;
 
-        while(!flag) {
+        while (!flag) {
             System.out.println("Type 'Start' to start the game, or 'Quit' to quit the game:");
             userInput = scanner.nextLine();
             if (userInput.equalsIgnoreCase("Start")) {
                 flag = true;
-            }
-            else if (userInput.equalsIgnoreCase("Quit")) {
+            } else if (userInput.equalsIgnoreCase("Quit")) {
                 System.exit(0);
-            }
-            else {
+            } else {
                 System.out.println("Not a valid input, please try again.");
             }
         }
 
+        flag = false;
         currentPlayer = game.playerList.get(0);
+        Command command;
 
         GAME:
         do {
             boolean nextPlayer = false;
 
             if (game.playerList.get(currentPlayer.getPlayerNumber()) == null) {
-                currentPlayer = game.playerList.get(currentPlayer.getPlayerNumber()+1);
-            }
-            else {
-                game.getBoard().printBoard();
+                currentPlayer = game.playerList.get(currentPlayer.getPlayerNumber() + 1);
+            } else {
+                while (!flag) {
+                    game.getBoard().printBoard();
 
-                System.out.println("It is Player " + (currentPlayer.getPlayerNumber() + 1) + "'s turn");
-                currentPlayer.displayHand();
-                System.out.println("What would you like to do? (play, exchange, shuffle, pass, forfeit)");
+                    System.out.println("It is Player " + (currentPlayer.getPlayerNumber() + 1) + "'s turn");
+                    currentPlayer.displayHand();
+                    System.out.println("What would you like to do? (play, exchange, shuffle, pass, forfeit)");
 
-                Command command = parser.getCommand();
+                    command = parser.getCommand();
 
+                    flag = true;
+                }
                 game.processCommand(command, currentPlayer);
+
+                if (currentPlayer.getPlayerNumber() == (game.playerList.size() - 1)) {
+                    currentPlayer = game.playerList.get(0);
+                } else {
+                    currentPlayer = game.playerList.get(currentPlayer.getPlayerNumber() + 1);
+                }
             }
 
-            if (currentPlayer.getPlayerNumber() == (game.playerList.size() - 1)) {
-                currentPlayer = game.playerList.get(0);
-            }
-            else {
-                currentPlayer = game.playerList.get(currentPlayer.getPlayerNumber()+1);
-            }
-
-        } while(running);
+        } while (running);
 
     }
 
