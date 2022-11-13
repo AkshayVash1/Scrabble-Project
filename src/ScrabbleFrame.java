473fadece4b00cdc3d70210d29af52d4b1049045
Mohamed Kaddour <mkaddour2001@gmail.com>
2022-11-13 13:37:37 -0500

    misc

--- src/ScrabbleFrame.java
+++ src/ScrabbleFrame.java
@@ -75,7 +75,6 @@ public class ScrabbleFrame extends JFrame {
         this.revalidate();
     }
 
-
     public static void main(String[] args) throws FileNotFoundException {
         ScrabbleFrame gameFrame = new ScrabbleFrame();
     }
