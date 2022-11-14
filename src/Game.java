01151721956b830bc7ae3cf28dd98089f816fa2b
Mohamed Kaddour <mkaddour2001@gmail.com>
2022-11-13 19:29:39 -0500

    Cleaned up Help text for JMenu

--- src/Game.java
+++ src/Game.java
@@ -34,7 +34,6 @@ public class Game {
         this.removeTilesFromHand= new ArrayList<>();
         this.exchangeTilesFromHand = new ArrayList<>();
         this.firstPlayInTurn = true;
-        this.printRules();
     }
 
     /**
@@ -159,33 +158,6 @@ public class Game {
     }
 
     /**
-     * Prints rules for the game
-     */
-    private void printRules() {
-        System.out.println("\n");
-        System.out.println("Welcome to Scrabble!\n");
-        System.out.println("------------------------------------------------");
-        System.out.println("Scrabble is a word game with 2-4 players. The goal of the game is to accumulate more " +
-                "than or equal to 50 points");
-        System.out.println("Each player starts with 7 tiles in their hand. All players have the 5 different actions " +
-                "they can perform: Play, Exchange, Pass, Shuffle and Forfeit.");
-        System.out.println("Play is used to place words from your hand onto the board (placing is done by typing " +
-                "'Play CAT (your word) A1 (the tile)'");
-        System.out.println("The order of the tile placement is important. Letter first means vertical placement and " +
-                "Number first means horizontal (A1) is vertical");
-        System.out.println("If the placement is illegal you will be asked to type a legal placement");
-        System.out.println("Exchange is used to exchange any letters in your hand for new ones from the bag. " +
-                "This is done by typing 'Exchange AO (words to be exchanged)'");
-        System.out.println("Pass will pass the current players turn. This is done by typing 'Pass'");
-        System.out.println("Shuffle will shuffle the current players hand so they can see their hand in a different " +
-                "order. This is done by typing 'Shuffle'");
-        System.out.println("Forfeit will have the player removed from the game " +
-                "and all of the player's tiles will be put back into the bag");
-        System.out.println("Good luck and have fun!");
-        System.out.println("------------------------------------------------");
-    }
-
-    /**
      * Takes in a command from a given player and performs an action based on the command
      * @param command Contains the different parts of the command the user entered
      * @return returns true of false (Should be void)
@@ -281,125 +253,6 @@ public class Game {
      */
     public Board getBoard() {
         return board;
-    }
-
-    /**
-     * gameLoop()
-     * @throws FileNotFoundException
-     */
-    public void gameLoop() throws FileNotFoundException {
-
-        //Game game = new Game();
-        Scanner scanner = new Scanner(System.in);
-        String userInput;
-        Player currentPlayer;
-        boolean flag = false;
-        Parser parser = new Parser();
-        int activeCount = 0;
-
-        boolean running = true;
-
-        this.printRules();
-
-        while(!flag) {
-            System.out.println("Type number of players (2, 3, or 4):");
-            userInput = scanner.nextLine();
-            if (userInput.equals("2") | userInput.equals("3") | userInput.equals("4") ) {
-                this.createPlayers(userInput);
-                flag = true;
-            }
-            else {
-                System.out.println("Not a valid input, please try again.");
-            }
-        }
-
-        flag = false;
-
-        while(!flag) {
-            System.out.println("Type 'Start' to start the game, or 'Quit' to quit the game:");
-            userInput = scanner.nextLine();
-            if (userInput.equalsIgnoreCase("Start")) {
-                flag = true;
-            }
-            else if (userInput.equalsIgnoreCase("Quit")) {
-                System.exit(0);
-            }
-            else {
-                System.out.println("Not a valid input, please try again.");
-            }
-        }
-
-        currentPlayer = this.playerList.get(0);
-        Command command = null;
-        flag = false;
-
-        GAME:
-        do {
-            activeCount = this.playerList.size();
-
-            if (currentPlayer.getPoints() >= 50) {
-                System.out.println("Player " + currentPlayer.getPlayerNumber() + " wins with "
-                        + currentPlayer.getPoints()+ "!");
-            }
-            for (int i = 0; i < this.playerList.size(); i++) {
-                if (!this.playerList.get(i).isActive()) {
-                    activeCount--;
-                }
-            }
-            if (activeCount == 1)
-            {
-                System.out.println("Not enough players");
-                running = false;
-            }
-            else if (!currentPlayer.isActive()) {
-                currentPlayer = this.playerList.get(currentPlayer.getPlayerNumber() + 1);
-            }
-            else {
-                this.getBoard().printBoard();
-
-                System.out.println("It is Player " + (currentPlayer.getPlayerNumber() + 1) + "'s turn");
-                System.out.println("Current Player Points: " + currentPlayer.getPoints());
-                currentPlayer.displayHand();
-                System.out.println("What would you like to do? (play, exchange, shuffle, pass, forfeit)");
-                while (!flag)
-                {
-                    command = parser.getCommand();
-                    if (!command.hasAction()) {
-                        System.out.println("Invalid Command");
-                    }
-                    else if (!command.hasWordAttempt() && ((command.getAction().equalsIgnoreCase("play") |
-                            (command.getAction().equalsIgnoreCase("exchange")))))
-                    {
-                        System.out.println("Word attempt is blank");
-                    }
-                    else if (!command.hasPlacementAttempt() &&
-                            ((command.getAction().equalsIgnoreCase("play"))))
-                    {
-                        System.out.println("Placement attempt is blank");
-                    }
-                    else {
-                        flag = true;
-                    }
-                }
-                flag = false;
-
-                //this.processCommand(command, currentPlayer);
-
-                if (!command.getAction().equals("shuffle")) {;
-                    if (!this.placementCheck) {
-                        System.out.println("Try again");
-                    }
-                    if (currentPlayer.getPlayerNumber() == (this.playerList.size()-1)) {
-                        currentPlayer = this.playerList.get(0);
-                    }
-                    else {
-                        currentPlayer = this.playerList.get(currentPlayer.getPlayerNumber()+1);
-                    }
-                }
-            }
-
-        } while(running);
-
     }
 
 }
--- src/GameConsolePanel.java
+++ src/GameConsolePanel.java
@@ -33,19 +33,28 @@ public class GameConsolePanel extends JPanel implements ScrabbleView {
         console.setSize(new Dimension(400, 500));
         console.setBackground(Color.lightGray);
         console.setText("Welcome to Scrabble! Scrabble is a word game for 2 to 4 Players." + NEWLINE +
-                "The goal of the game is to accumulate more than or equal to 50 points"  + NEWLINE2);
+                "The goal of the game is to accumulate more than or equal to 50 points."  + NEWLINE2 +
+                "Press help for details." + NEWLINE2);
 
         this.setPreferredSize(new Dimension(400, 500));
         this.setVisible(true);
     }
 
-    public JTextArea getConsole() {
-        return console;
+    public void appendHelp()
+    {
+        console.append("Scrabble is a word game with 2-4 players. The goal of the game is to accumulate more than or equal to 50 points \n" +
+                "Each player starts with 7 tiles in their hand. All players have the 5 different actions \n" +
+                "they can perform: Play, Exchange, Pass, Shuffle \n" +
+                "To start, drag and drop the tiles you want to play to the board IN THE CORRECT WORD ORDER. Once all the tiles\n" +
+                "are placed, you can press play and if the play and word is valid, the next player's turn will happen\n" +
+                "You can also shuffle, pass or press exchange and select which tiles you would like to exchange\n\n");
+
+        textCount += 8;
     }
 
     @Override
     public void update(Player currentPlayer, Board board) {
-        if (textCount == 12) {
+        if (textCount > 12) {
             console.setText("");
             textCount = 0;
         }
